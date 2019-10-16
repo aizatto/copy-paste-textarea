e305eded00debfc980543fddebbd7fdaec0a9ec4
Aizat Faiz <aizat.faiz@gmail.com>
2019-10-16 14:43:10 +0800

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f5e4563eb34edc4ceb90";
+/******/ 	var hotCurrentHash = "edfc08c140984777d412";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
